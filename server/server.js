7e32efffe10bedf1ebdc38dbfbc16bf3b2f3c910
Jaxon Avena <jaxonavena@gmail.com>
2025-02-08 17:31:04 -0600

    Fruits :D

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.get("/api", (req, res) => {
   res.json({"fruits": ["apple", "orange", "banana"]});
 });
 
+app.get("/jaxon", (req, res) => {
+  res.json({"fruits": ["jaxon", "drew", "ach"]});
+});
+
 let port = 8080;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
